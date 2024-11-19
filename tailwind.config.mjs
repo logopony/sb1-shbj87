42f2ede96819b8c417caebbdfccb90a5f85f7993
logopony <git@stackblitz.com>
2024-11-19 16:36:35 +0200

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4ead87',
+        dark: '#000000'
+      }
+    },
+  },
+  plugins: [],
+}
